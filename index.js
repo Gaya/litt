417f6074885097688b15d7e72f854c34f9a382f8
Gaya Kessler <gaya@theclevernode.com>
2022-07-26 13:44:26 +0200

    report the port

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
   console.log(req);
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Litt is a go!');
+app.listen(port, () => {
+  console.log(`Litt is a go on port ${port}`);
 });
