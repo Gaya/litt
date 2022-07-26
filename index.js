267d11701773c5817c85d9c336a0509ac38e24b8
Gaya Kessler <gaya@theclevernode.com>
2022-07-26 13:48:05 +0200

    add checks and engines

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.get('/check', (req, res) => {
+  res.end('OK');
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
   console.log(req);
