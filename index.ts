a83d4c7dbb5f05197d86ee9098564081452a4d03
Gaya Kessler <gaya@theclevernode.com>
2022-07-26 15:54:50 +0200

    clean up structure

--- index.ts
+++ index.ts
@@ -1,27 +1,3 @@
 import 'dotenv/config';
-import express from 'express';
 
-const app = express();
-
-const PORT = process.env.PORT || 3000;
-const BASE_HOSTNAME = process.env.BASE_HOSTNAME || 'localhost';
-
-app.get('/check', (req, res) => {
-  res.end('OK');
-});
-
-app.get('/', (req, res, next) => {
-  const folder = req.hostname === BASE_HOSTNAME
-    ? 'static' : `sites/${req.hostname}`;
-
-  express.static(folder)(req, res, next);
-});
-
-app.use((req, res) => {
-  res.status(404)
-    .sendFile('static/404.html', { root: __dirname });
-})
-
-app.listen((PORT), () => {
-  console.log(`Litt is a go on port ${PORT}`);
-});
+import './src/server';
---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import path from 'path';
+import express, { RequestHandler } from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const BASE_HOSTNAME = process.env.BASE_HOSTNAME || 'localhost';
+
+const appCheck: RequestHandler = (req, res) => {
+  res.end('OK');
+};
+
+const serveStatic: RequestHandler = (req, res, next) => {
+  const folder = req.hostname === BASE_HOSTNAME
+    ? 'static' : `sites/${req.hostname}`;
+
+  express.static(path.join(__dirname, '..', folder))(req, res, next);
+};
+
+const handleNotFound: RequestHandler = (req, res) => {
+  res.status(404)
+    .sendFile(path.join(__dirname, '../static/404.html'));
+};
+
+app.get('/check', appCheck);
+app.use(serveStatic);
+app.use(handleNotFound);
+
+app.listen(PORT, () => {
+  console.log(`Litt is a go on port ${PORT}`);
+});
